36a8df3a7904dd1abb4d45e025d589409213a162
Jhonny <joaopedross3@hotmail.com>
2025-11-09 13:07:31 -0300

    fix: tentando corrigir ainda alguns erros de desempenho

--- src/components/ChurchHero.tsx
+++ src/components/ChurchHero.tsx
@@ -38,6 +38,7 @@ const ChurchHero = () => {
               fetchPriority="high"
               width="128"
               height="128"
+              loading="lazy"
             />
           </div>
           
